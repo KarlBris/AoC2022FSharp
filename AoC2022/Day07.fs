99494c42b24d1716a25aa0e50a0ca2d7a21fc6e6
Karl Bristav <karl.bristav@antura.se>
2022-12-07 13:20:37 +0100

    Day 7 nicer

--- AoC2022/Day07.fs
+++ AoC2022/Day07.fs
@@ -5,63 +5,75 @@ open Utils
 module Day07 =
 
     type DirTree =
-        | Directory of (string * DirTree list * int option)
+        | Directory of (string * DirTree list * int)
         | File of (string * int)
 
-    let makePathList (path: string) : string list =
-        let a = slashes path |> Array.toList
-        a
+    let makePathList (path: string) : string list = path |> slashes |> Array.toList
 
     let rec getDirectoryTree (path: string list) (tree: DirTree) : DirTree option =
         match path with
         | [] -> Some tree
-        | p::ps -> 
+        | p :: ps ->
             match tree with
             | File _ -> None
-            | Directory (name, subDirs, _) ->
-                let subTreeOption = subDirs |> List.filter (fun d ->
-                                                        match d with
-                                                        | File _ -> false
-                                                        | Directory (name,_,_) -> name = p)
-                                      |> List.tryHead
+            | Directory (_, subDirs, _) ->
+                let subTreeOption =
+                    subDirs
+                    |> List.filter (fun d ->
+                        match d with
+                        | File _ -> false
+                        | Directory (name, _, _) -> name = p)
+                    |> List.tryHead
 
                 match subTreeOption with
                 | Some subTree -> getDirectoryTree ps subTree
                 | None -> None
-                
+
     let replaceSubTree (newSubTree: DirTree) (subTreeList: DirTree list) : (DirTree list) =
-        let newSubTreeName = match newSubTree with
-                             | File (name, _) -> name
-                             | Directory (name, _, _) -> name
-        List.map (fun stree -> match stree with
-                               | Directory (name,_,_) as dir -> if name = newSubTreeName then newSubTree else dir
-                               | file -> file) subTreeList
+        let newSubTreeName =
+            match newSubTree with
+            | File (name, _) -> name
+            | Directory (name, _, _) -> name
+
+        List.map
+            (fun stree ->
+                match stree with
+                | Directory (name, _, _) as dir ->
+                    if name = newSubTreeName then
+                        newSubTree
+                    else
+                        dir
+                | file -> file)
+            subTreeList
 
     let rec setDirectoryTree (path: string list) (newSubTree: DirTree) (tree: DirTree) : DirTree =
         match path with
-        | [] -> match tree with
-                | File _ -> tree
-                | Directory (name, subs, size) ->
-                        newSubTree
-        | p::ps -> 
+        | [] ->
             match tree with
-                | File _ -> failwith "fail"
-                | Directory (name, subs, size) as dir ->
-                    let subTreeOption = subs |> List.filter (fun d ->
-                                                    match d with
-                                                    | File _ -> false
-                                                    | Directory (name,_,_) -> name = p)
-                                             |> List.tryHead
-                    match subTreeOption with
-                    | Some subTree -> 
-                        let newSubTrees = replaceSubTree (setDirectoryTree ps newSubTree subTree) subs
-                        Directory (name, newSubTrees, size)
-                    | None -> dir
+            | File _ -> tree
+            | Directory _ -> newSubTree
+        | p :: ps ->
+            match tree with
+            | File _ -> failwith "fail"
+            | Directory (name, subs, size) as dir ->
+                let subTreeOption =
+                    subs
+                    |> List.filter (fun d ->
+                        match d with
+                        | File _ -> false
+                        | Directory (name, _, _) -> name = p)
+                    |> List.tryHead
+
+                match subTreeOption with
+                | Some subTree ->
+                    let newSubTrees = replaceSubTree (setDirectoryTree ps newSubTree subTree) subs
+                    Directory(name, newSubTrees, size)
+                | None -> dir
 
     let addToCurrentDirectory (currentPath: string) (tree: DirTree) (toAdd: DirTree) : DirTree =
         match getDirectoryTree (makePathList currentPath) tree with
         | None -> failwith "could not find current directory"
-        | Some (File _) -> failwith "file got instead of directory"
+        | Some (File _) -> failwith "file found instead of directory"
         | Some (Directory (name, subs, size)) ->
             setDirectoryTree (makePathList currentPath) (Directory(name, toAdd :: subs, size)) tree
 
@@ -73,15 +85,12 @@ module Day07 =
             | Directory (name, _, _) -> [ name ])
         |> List.concat
 
-    let oneStepUp (path: string) : string=
-        let a = path
-                |> slashes
-                |> Array.toList
-                |> List.rev
+    let oneStepUp (path: string) : string =
+        let a = path |> slashes |> Array.toList |> List.rev
 
         match a with
         | [] -> "/"
-        | d::ds -> "/" + (String.concat "/" (List.rev ds))
+        | _ :: ds -> "/" + (String.concat "/" (List.rev ds))
 
     let rec parseTree (currentPath: string) (tree: DirTree) (lines: string list) : DirTree =
         match lines with
@@ -94,17 +103,13 @@ module Day07 =
                 match lineWords[1] with
                 | "cd" ->
                     match lineWords[2] with
-                    | ".." ->
-                        parseTree (oneStepUp currentPath) tree restOfLines
+                    | ".." -> parseTree (oneStepUp currentPath) tree restOfLines
                     | dir -> parseTree (currentPath + "/" + dir) tree restOfLines
                 | _ -> parseTree currentPath tree restOfLines // ls case
             | "dir" ->
-                let directory = (lineWords[1], [], None)
+                let directory = (lineWords[1], [], 0)
 
-                parseTree
-                    currentPath
-                    (addToCurrentDirectory currentPath tree (Directory directory))
-                    restOfLines
+                parseTree currentPath (addToCurrentDirectory currentPath tree (Directory directory)) restOfLines
             | _other ->
                 let file = (lineWords[1], int lineWords[0])
                 parseTree currentPath (addToCurrentDirectory currentPath tree (File file)) restOfLines
@@ -116,10 +121,10 @@ module Day07 =
 
     let rec setWeights (tree: DirTree) : DirTree =
         match tree with
-        | Directory (name, subs, size) ->
+        | Directory (name, subs, _) ->
             let newSubTree = subs |> List.map setWeights
             let newSize = subs |> List.map getSize |> List.sum
-            Directory(name, newSubTree, Some newSize)
+            Directory(name, newSubTree, newSize)
         | file -> file
 
     let rec findSumOfDirsOfAtMostSize (maxSize: int) (tree: DirTree) : int =
@@ -130,43 +135,36 @@ module Day07 =
                 |> List.map (findSumOfDirsOfAtMostSize maxSize)
                 |> List.sum
 
-            a + (Option.get size)
+            a + size
         | _ -> 0
 
-    let rec getDirectoryList (tree: DirTree) : DirTree list =
+    let rec getDirectorySizeList (tree: DirTree) : int list =
         match tree with
         | File _ -> []
-        | Directory (_, subs, _) as dir ->
-            dir
-            :: (subs |> List.map getDirectoryList |> List.concat)
+        | Directory (_, subs, size) ->
+            size
+            :: (subs
+                |> List.map getDirectorySizeList
+                |> List.concat)
+
+    let parseTreeAndDistributeWeights (input: string) : int list =
+        input
+        |> lines
+        |> Array.toList
+        |> parseTree "/" (Directory("/", [], 0))
+        |> setWeights
+        |> getDirectorySizeList
 
     let part1 (input: string) : string =
-        let tree =
-            input
-            |> lines
-            |> Array.toList
-            |> parseTree "/" (Directory("/", [], None))
-
-        let weightedTree = setWeights tree
-        let dirList = getDirectoryList weightedTree
-
-        dirList
-        |> List.map (fun (Directory (_, _, size)) -> size)
-        |> List.filter (fun s -> Option.get s <= 100000)
-        |> List.map Option.get
+        input
+        |> parseTreeAndDistributeWeights
+        |> List.filter (fun s -> s <= 100000)
         |> List.sum
         |> string
 
-    let part2 (input: string) : string = 
-        let tree =
-            input
-            |> lines
-            |> Array.toList
-            |> parseTree "/" (Directory("/", [], None))
+    let part2 (input: string) : string =
+        let dirList = input |> parseTreeAndDistributeWeights
 
-        let weightedTree = setWeights tree
-        let dirList = weightedTree |> getDirectoryList |> List.map (fun (Directory (_, _, size)) -> Option.get size)
-                        
         let freeSpace = (70000000 - (List.max dirList))
         let spaceNeeded = 30000000 - freeSpace
 
